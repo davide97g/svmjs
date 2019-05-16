6334a4eccd5763cd06809e9bdb4c7fdd3c1b9eda
Davide Ghiotto <45457304+davide97g@users.noreply.github.com>
2019-05-16 18:26:39 +0200

    file separation

---
+++ pratice/js/functions.js
@@ -0,0 +1,72 @@
+$(function() {
+    // for C parameter
+    $("#slider1").slider({
+        orientation: "horizontal",
+        slide: refreshC,
+        max: 2.0,
+        min: -2.0,
+        step: 0.1,
+        value: 0.0
+    });
+    
+    // for rbf kernel sigma
+    $("#slider2").slider({
+        orientation: "horizontal",
+        slide: refreshSig,
+        max: 2.0,
+        min: -2.0,
+        step: 0.1,
+        value: 0.0
+    });
+    
+    // for polynomial kernel degree
+    $("#slider3").slider({
+        orientation: "horizontal",
+        slide: refreshDegree,
+        max: 5.0,
+        min: 1.0,
+        step: 1.0,
+        value: 2.0
+    });
+    
+    // for polynomial kernel influence
+    $("#slider4").slider({
+        orientation: "horizontal",
+        slide: refreshInfluence,
+        max: 3.0,
+        min: 0.0,
+        step: 0.1,
+        value: 0.0
+    });
+    
+    // for upper bound in SSCA
+    $("#slider5").slider({
+        orientation: "horizontal",
+        slide: refreshUpperBound,
+        max: 1.5,
+        min: 0.0,
+        step: 0.1,
+        value: 1.3
+    });
+    
+    // for K in KNN
+    $("#slider6").slider({
+        orientation: "horizontal",
+        slide: refreshK,
+        max: 15,
+        min: 1,
+        step: 1,
+        value: 1
+    });
+    
+    // for P in minkowski distance
+    $("#slider7").slider({
+        orientation: "horizontal",
+        slide: refreshP,
+        max: 10,
+        min: 1,
+        step: 1,
+        value: 2
+    });
+    
+});
